e45258d238694ff371a7ce4aafd1cb98a6deb0f5
TriPham0501 <tripham0501@gmail.com>
2024-10-16 21:49:10 +0700

    add coding notes

--- Bai4/Bai4_TriPham.asm
+++ Bai4/Bai4_TriPham.asm
@@ -2,18 +2,18 @@
 .text
 .globl main
 main:
-	li $s0, 99             #Load count value
-	li $t1, 0x10010000     #Load address of destination array
+	li $s0, 99             # Load count value
+	li $t1, 0x10010000     # Load address of destination array
 	
 	Loop:
-		beq $s0, $zero, End    #compare if count value equal 0 then jump to End
+		beq $s0, $zero, End    # compare if count value equal 0 then jump to End
 		      
-                sw $s0, 0($t1)         #save value into a destination array
-    		addi $t1, $t1, 4       #move 4 bits forward to store next value                
+                sw $s0, 0($t1)         # save value into the register of destination array
+    		addi $t1, $t1, 4       # move 4 bits forward to store next value                
 
-		sub $s0, $s0, 1        #subtract count value by one 
-		j Loop                 #jump to Loop to repeat the cycle
+		sub $s0, $s0, 1        # subtract count value by one 
+		j Loop                 # jump to Loop to repeat the cycle
 		
 	End:
-		li $v0, 10             #End the program
+		li $v0, 10             # End the program
 		syscall
